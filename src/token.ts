11c2e73ab13370aaebd4e512146b5dc065f98f21
Tim Garthwaite <tim@fabrica.land>
2022-12-12 12:49:52 -0500

    Initial commit

--- src/token.ts
+++ src/token.ts
@@ -1,88 +1,70 @@
+import { Bytes } from '@graphprotocol/graph-ts'
 import {
-  ApprovalForAll as ApprovalForAllEvent,
-  Creation as CreationEvent,
+  Transfer,
+  Token,
+  User
+} from '../generated/schema'
+import {
   TransferBatch as TransferBatchEvent,
   TransferSingle as TransferSingleEvent,
-  URI as URIEvent
-} from "../generated/Token/Token"
-import {
-  ApprovalForAll,
-  Creation,
-  TransferBatch,
-  TransferSingle,
-  URI
-} from "../generated/schema"
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCreation(event: CreationEvent): void {
-  let entity = new Creation(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.uri = event.params.uri
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+} from '../generated/Token/Token'
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
-  let entity = new TransferBatch(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  for (let i = 0 ; i < event.params.ids.length ; i++) {
+    const transfer = new Transfer(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    const tokenIdBytes = Bytes.fromByteArray(Bytes.fromBigInt(event.params.ids[i]))
+    transfer.operator = event.params.operator
+    transfer.from = event.params.from
+    transfer.to = event.params.to
+    transfer.token = tokenIdBytes
+    transfer.blockNumber = event.block.number
+    transfer.blockTimestamp = event.block.timestamp
+    transfer.transactionHash = event.transaction.hash
+    transfer.save()
+    let token = Token.load(tokenIdBytes)
+    if (token == null) {
+      token = new Token(tokenIdBytes)
+      token.creator = event.params.to
+      token.tokenId = event.params.ids[i]
+      token.totalSupply = event.params.values[i]
+    }
+    token.owner = event.params.to
+    token.save()
+    let user = User.load(event.params.to)
+    if (user == null) {
+      user = new User(event.params.to)
+      user.save()
+    }
+  }
 }
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  const transfer = new Transfer(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.id = event.params.id
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  const tokenIdBytes = Bytes.fromByteArray(Bytes.fromBigInt(event.params.id))
+  transfer.operator = event.params.operator
+  transfer.from = event.params.from
+  transfer.to = event.params.to
+  transfer.token = tokenIdBytes
+  transfer.blockNumber = event.block.number
+  transfer.blockTimestamp = event.block.timestamp
+  transfer.transactionHash = event.transaction.hash
+  transfer.save()
+  let token = Token.load(tokenIdBytes)
+  if (token == null) {
+    token = new Token(tokenIdBytes)
+    token.creator = event.params.to
+    token.tokenId = event.params.id
+    token.totalSupply = event.params.value
+  }
+  token.owner = event.params.to
+  token.save()
+  let user = User.load(event.params.to)
+  if (user == null) {
+    user = new User(event.params.to)
+    user.save()
+  }
 }
