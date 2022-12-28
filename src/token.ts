7f0c016803be2c0d731c82e4a7a5d5b4ad41e305
Tim Garthwaite <tim@fabrica.land>
2022-12-28 17:25:22 -0500

    Replace owner with balances

--- src/token.ts
+++ src/token.ts
@@ -1,8 +1,8 @@
-import { Bytes } from '@graphprotocol/graph-ts'
+import {Address, BigInt, Bytes} from '@graphprotocol/graph-ts'
 import {
   Transfer,
   Token,
-  User
+  User, Balance
 } from '../generated/schema'
 import {
   Token as TokenContract,
@@ -27,7 +27,23 @@ export function handleTransferBatch(event: TransferBatchEvent): void {
     transfer.blockNumber = event.block.number
     transfer.blockTimestamp = event.block.timestamp
     transfer.transactionHash = event.transaction.hash
+    transfer.value = event.params.values[i]
     transfer.save()
+    let operator = User.load(event.params.operator)
+    if (operator == null) {
+      operator = new User(event.params.operator)
+      operator.save()
+    }
+    let from = User.load(event.params.from)
+    if (from == null) {
+      from = new User(event.params.from)
+      from.save()
+    }
+    let to = User.load(event.params.from)
+    if (to == null) {
+      to = new User(event.params.from)
+      to.save()
+    }
     const tokenContract = TokenContract.bind(event.address)
     const properties = tokenContract._property(event.params.ids[i])
     let token = Token.load(tokenIdBytes)
@@ -36,19 +52,40 @@ export function handleTransferBatch(event: TransferBatchEvent): void {
       token.creator = event.params.to
       token.definition = properties.getDefinition()
       token.tokenId = event.params.ids[i]
-      token.totalSupply = event.params.values[i]
     }
     token.configuration = properties.getConfiguration()
     token.operatingAgreement = properties.getOperatingAgreement()
     token.validator = properties.getValidator()
-    token.owner = event.params.to
     token.uri = tokenContract.uri(event.params.ids[i])
-    token.save()
-    let user = User.load(event.params.to)
-    if (user == null) {
-      user = new User(event.params.to)
-      user.save()
+    if (event.params.from == Address.zero()) {
+      token.totalSupply = event.params.values[i]
+    } else {
+      let balance = Balance.load(event.params.from.concat(tokenIdBytes))
+      if (balance == null) {
+        balance = new Balance(event.params.from.concat(tokenIdBytes))
+        balance.token = tokenIdBytes
+        balance.holder = event.params.from
+        balance.balance = BigInt.zero()
+      } else {
+        balance.balance = balance.balance.minus(event.params.values[i])
+      }
+      balance.save()
     }
+    if (event.params.to == Address.zero()) {
+      token.totalSupply = token.totalSupply.minus(event.params.values[i])
+    } else {
+      let balance = Balance.load(event.params.to.concat(tokenIdBytes))
+      if (balance == null) {
+        balance = new Balance(event.params.to.concat(tokenIdBytes))
+        balance.token = tokenIdBytes
+        balance.holder = event.params.to
+        balance.balance = event.params.values[i]
+      } else {
+        balance.balance = balance.balance.plus(event.params.values[i])
+      }
+      balance.save()
+    }
+    token.save()
   }
 }
 
@@ -64,7 +101,23 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
   transfer.blockNumber = event.block.number
   transfer.blockTimestamp = event.block.timestamp
   transfer.transactionHash = event.transaction.hash
+  transfer.value = event.params.value
   transfer.save()
+  let operator = User.load(event.params.operator)
+  if (operator == null) {
+    operator = new User(event.params.operator)
+    operator.save()
+  }
+  let from = User.load(event.params.from)
+  if (from == null) {
+    from = new User(event.params.from)
+    from.save()
+  }
+  let to = User.load(event.params.to)
+  if (to == null) {
+    to = new User(event.params.to)
+    to.save()
+  }
   const tokenContract = TokenContract.bind(event.address)
   const properties = tokenContract._property(event.params.id)
   let token = Token.load(tokenIdBytes)
@@ -73,19 +126,40 @@ export function handleTransferSingle(event: TransferSingleEvent): void {
     token.creator = event.params.to
     token.definition = properties.getDefinition()
     token.tokenId = event.params.id
-    token.totalSupply = event.params.value
   }
   token.configuration = properties.getConfiguration()
   token.operatingAgreement = properties.getOperatingAgreement()
   token.validator = properties.getValidator()
-  token.owner = event.params.to
   token.uri = tokenContract.uri(event.params.id)
-  token.save()
-  let user = User.load(event.params.to)
-  if (user == null) {
-    user = new User(event.params.to)
-    user.save()
+  if (event.params.from == Address.zero()) {
+    token.totalSupply = event.params.value
+  } else {
+    let balance = Balance.load(event.params.from.concat(tokenIdBytes))
+    if (balance == null) {
+      balance = new Balance(event.params.from.concat(tokenIdBytes))
+      balance.token = tokenIdBytes
+      balance.holder = event.params.from
+      balance.balance = BigInt.zero()
+    } else {
+      balance.balance = balance.balance.minus(event.params.value)
+    }
+    balance.save()
   }
+  if (event.params.to == Address.zero()) {
+    token.totalSupply = token.totalSupply.minus(event.params.value)
+  } else {
+    let balance = Balance.load(event.params.to.concat(tokenIdBytes))
+    if (balance == null) {
+      balance = new Balance(event.params.to.concat(tokenIdBytes))
+      balance.token = tokenIdBytes
+      balance.holder = event.params.to
+      balance.balance = event.params.value
+    } else {
+      balance.balance = balance.balance.plus(event.params.value)
+    }
+    balance.save()
+  }
+  token.save()
 }
 
 export function handleUriChanged(event: UriEvent): void {
